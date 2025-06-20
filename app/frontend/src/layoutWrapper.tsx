a196ac4d16dc41b32b9843ef63b630717103d17f
samempy105 <sidnani@empyreanit.com.au>
2025-06-20 14:26:54 +1000

    Update layoutWrapper.tsx

--- app/frontend/src/layoutWrapper.tsx
+++ app/frontend/src/layoutWrapper.tsx
@@ -1,3 +1,4 @@
+import appLogo from "./assets/munro-logo.png";
 import { AccountInfo, EventType, PublicClientApplication } from "@azure/msal-browser";
 import { checkLoggedIn, msalConfig, useLogin } from "./authConfig";
 import { useEffect, useState } from "react";
@@ -8,7 +9,7 @@ import Layout from "./pages/layout/Layout";
     const LayoutWrapper = () => {
     const [loggedIn, setLoggedIn] = useState(false);
     const uiTitle = import.meta.env.VITE_UI_TITLE || "My App";
-    const uiLogo = import.meta.env.VITE_UI_LOGO;
+    const uiLogo = appLogo;
     if (useLogin) {
         var msalInstance = new PublicClientApplication(msalConfig);
 
