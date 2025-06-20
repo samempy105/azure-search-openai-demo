3d3519cc26ee653120b2fc9a3fc9c4e99f14f31e
samempy105 <sidnani@empyreanit.com.au>
2025-06-20 14:44:23 +1000

    Update Chat.tsx

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -419,8 +419,8 @@ interface ChatProps {
                         <div className={styles.chatEmptyState}>
                             {uiLogo && <img src={uiLogo} alt="App logo" width="120" height="120" />}
 
-                            <h1 className={styles.chatEmptyStateTitle}>"Ask Lacey"</h1>
-                            <h2 className={styles.chatEmptyStateSubtitle}>{t("chatEmptyStateSubtitle")}</h2>
+                            <h1 className={styles.chatEmptyStateTitle}>Ask Lacey</h1>
+                            <h2 className={styles.chatEmptyStateSubtitle}> </h2>
                             {showLanguagePicker && <LanguagePicker onLanguageChange={newLang => i18n.changeLanguage(newLang)} />}
                         </div>
                     ) : (
